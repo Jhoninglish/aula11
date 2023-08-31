a8d3b06b64d63f27d796325f1e7ccf57f2c833d6
Jhoninglish <142617436+Jhoninglish@users.noreply.github.com>
2023-08-31 17:27:00 -0300

    Create aula11.js

---
+++ aula11.js
@@ -0,0 +1,81 @@
+// this is a js file
+// João Vítor da Silva
+
+/*
+----------------------------------------------------------------------------------------------------
+//1.resposta: No retornara a função 5 e multiplicara ela por 10 e mostrara no console
+o resultado que é 50. Se removesse ambos os console.log ele não imprimiria o resultado no console.
+
+----------------------------------------------------------------------------------------------------
+//2.resposta: Esta função pegará o texto do usuario e verificará se possue a palavra "cenoura",
+se não tiver o texto será dito como false, se tiver a palavra "cenoura" será true.
+ Depois ele executará a função resposta que é false ou true e mostrará no console.
+ i = true   iii = false
+ ii = true
+----------------------------------------------------------------------------------------------------
+//3.resposta: 
+function imprimirOlá() {
+    console.log("Olá, sou João tenho 15 anos, moro em São Leopoldo e sou estudante.");
+}
+imprimirOlá()
+
+----------------------------------------------------------------------------------------------------
+//4.resposta:
+function imprimirInfo(nome, idade, endereco, profissao) {
+return(`Olá sou ${Nome}, tenho ${Idade} anos, moro em ${Endereço} e minha profissão é ${Profissão}.`);
+
+}
+
+-----------------------------------------------------------------------------------------------------
+//5.resposta:
+let Numero1 = Number(prompt("Digite um numero"));
+let Numero2 = Number(prompt("Digite outro numero"));
+
+function calcularsoma(Numero1, Numero2) {
+    const soma = Numero1 + Numero2
+    console.log (soma);
+}
+calcularsoma(Numero1, Numero2);
+
+
+//B.resposta:
+let a = Number(prompt("Digite um numero"));
+let b = Number(prompt("Digite outro numero"));
+(a >= b );
+console.log(a >= b);
+ 
+
+//C.resposta:
+function verificaPar(a) {
+     return (a % 2 === 0);
+}
+
+//D.resposta:
+function tamanhoEmaiusculas(a){
+    return a.length + " " + a.toUpperCase();
+}
+let frase1 = prompt("Digite uma frase");
+console.log(tamanhoEmaiusculas(frase1))
+
+----------------------------------------------------------------------------------------------------
+//6.resposta:
+function soma(a, b) {
+    return a + b;
+}
+function subtracao(a, b) {
+    return a - b;
+}
+function multiplicacao(a, b) {
+    return a * b;
+}
+function divisao(a, b) {
+    return a / b;
+let a = Number(prompt("Digite o primeiro numero"));
+let b = Number(prompt("Digite o segundo numero"));
+    
+console.log(soma(a, b));
+console.log(subtracao(a, b));
+console.log(multiplicacao(a, b));
+console.log(divisao(a, b));
+----------------------------------------------------------------------------------------------------
+*/
